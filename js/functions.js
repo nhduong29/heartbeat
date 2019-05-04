9a8ce7a412c759c31ae2f9e2ca51251494afa515
nhduong29 <nhduong29@gmail.com>
2019-05-04 21:27:59 +0700

    js mobile

--- js/functions.js
+++ js/functions.js
@@ -60,6 +60,26 @@ $(window).on('resize', function(){
   calHeight()
 });
 
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  // Windows Phone must come first because its UA also contains "Android"
+  if (/windows phone/i.test(userAgent)) {
+    return "Windows Phone";
+  }
+
+  if (/android/i.test(userAgent)) {
+    return "Android";
+  }
+
+  // iOS detection from: http://stackoverflow.com/a/9039885/177710
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return "iOS";
+  }
+
+  return "unknown";
+}
+
 $(document).ready( function(){
   Amplitude.init({
     "bindings": {
@@ -70,8 +90,11 @@ $(document).ready( function(){
     "songs": songs
   });
   calHeight();
-  $("#list").mCustomScrollbar({
-    theme: "minimal",
-    scrollInertia: 200
-  });
+  let os = getMobileOperatingSystem();
+  if(os == "unknown"){
+    $("#list").mCustomScrollbar({
+      theme: "minimal",
+      scrollInertia: 200
+    });
+  }
 });
